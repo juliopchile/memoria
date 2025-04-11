5ed114e915e1418b29910389c5ac3b1eb8e966b8
memristor <julio.lopezb@sansano.usm.cl>
2025-04-11 00:11:15 -0400

    No se

--- utility_training.py
+++ utility_training.py
@@ -463,7 +463,7 @@ if __name__ == "__main__":
     extra_params = {"epochs": 80, "batch": 8}
     first_run_json = "training/run1.json"
     second_run_json = "training/run2.json"
-    
+
     #? 1) Primero creamos los archivos JSON que contienen los parámetros de entrenamiento para cada caso.
     def first_experiment_json():
         #* I) El primer entrenamiento será con todo en default.
@@ -473,7 +473,7 @@ if __name__ == "__main__":
         create_training_json(dataset_yaml_list=dataset_yaml_list, model_name_list=models_to_use, optimizer_list=optimizers,
                             project_name=project_name , freeze_dict=freeze_dict, json_file=first_run_json,
                             extra_params=extra_params)
-        
+
         #* II) El segundo entrenamiento será igual pero con transfer learning (congelar backbone)
         freeze_dict = {'yolov8n-seg': True, 'yolov8s-seg': True, 'yolov8m-seg': True, 'yolov8l-seg': True,
                     'yolov8x-seg': True, 'yolov9c-seg': True, 'yolov9e-seg': True, 'yolo11n-seg': True,
@@ -488,9 +488,9 @@ if __name__ == "__main__":
     # train_run(config_file=second_run_json)
 
     #? 3) Exportamos los entrenamientos con TensorRT
-    # export_experiments(first_run_json)
-    # export_experiments(second_run_json)
-    
+    #export_experiments(first_run_json)
+    #export_experiments(second_run_json)
+
     #? 4) Realizamos validación para todos los modelos entrenados y exportados.
-    # validate_run(first_run_json, "training/results_1.csv")
-    # validate_run(second_run_json, "training/results_2.csv")
+    #validate_run(first_run_json, "training/results_1.csv")
+    #validate_run(second_run_json, "training/results_2.csv")
